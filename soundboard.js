c431b66ca9bc8a34388c0fb5d17ef99102ff6e04
Brian Baxter <baxterb6@msu.edu>
2020-11-11 00:12:43 -0500

    Bloop

--- soundboard.js
+++ soundboard.js
@@ -1,20 +1,6 @@
-
-var bloopButton = document.getElementById("Bloop");
-bloopButton.addEventListener("click",playBloop());
-
-var clickButton = document.getElementById("Click");
-clickButton.addEventListener("click",function(){
-    audio.play(click);
-})
-
-let playBlip = function(){document.getElementById("Blip").play()}
-
 function playBloop(){
-    var bloop = new Audio("bloop_x.wav");
-    audio.play(bloop);
+    var bloop = new Audio();
+    bloop.src="bloop_x.wav";
+    bloop.play();
 }
-
-function playClick(){
-    var click = new Audio("click_x.wav");
-    audio.play();
-}
+document.getElementById("Bloop").onclick = playBloop;
