b5fd222cbdde359c5405ffae891304a3c5924e40
Brian Baxter <baxterb6@msu.edu>
2020-11-11 00:14:44 -0500

    Audio fixed

--- soundboard.js
+++ soundboard.js
@@ -4,3 +4,17 @@ function playBloop(){
     bloop.play();
 }
 document.getElementById("Bloop").onclick = playBloop;
+
+function playBlip(){
+    var blip = new Audio();
+    blip.src="blip.wav";
+    blip.play();
+}
+document.getElementById("Blip").onclick = playBlip;
+
+function playClick(){
+    var click = new Audio();
+    click.src="click_x.wav";
+    click.play();
+}
+document.getElementById("Click").onclick = playClick;
